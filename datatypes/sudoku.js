b0dcef61fe6059930b7fbf25b68f4c64d4a31f9a
Yichuan Shen <shen.yichuan@gmail.com>
2017-03-08 20:46:31 +0100

    Add Sudoku.getBoxes()

--- datatypes/sudoku.js
+++ datatypes/sudoku.js
@@ -1,7 +1,23 @@
 class Sudoku {
     constructor() {
-        this.arrangement = [...Array(9)].map(_ => Array(9).fill(null))
+        this.arrangement = [...Array(9)].map(_ => [...Array(9)].map(__ => []))
+        this.excluded = [...Array(9)].map(_ => [...Array(9)].map(__ => []))
         this.solids = []
+    }
+
+    getBoxes() {
+        let tuples = arr => arr.map(x => arr.map(y => [x, y])).reduce((sum, x) => [...sum, ...x], [])
+
+        return [
+            // Rows
+            ...[...Array(9)].map((_, i) => [...Array(9)].map((__, j) => [i, j])),
+            // Columns
+            ...[...Array(9)].map((_, i) => [...Array(9)].map((__, j) => [j, i])),
+            // Squares
+            ...tuples([0, 3, 6]).map(([tx, ty]) =>
+                [...Array(3)].map((_, i) => [...Array(3)].map((__, j) => [i + ty, j + tx]))
+            )
+        ]
     }
 }
 
