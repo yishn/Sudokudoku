9e8826bcd4358cb83213d6239b00d761708cf61a
Yichuan Shen <shen.yichuan@gmail.com>
2017-03-09 00:43:59 +0100

    Add SudokuGrid component

--- components/App.js
+++ components/App.js
@@ -1,11 +1,10 @@
 const {h, Component} = require('preact')
 const SudokuGrid = require('./SudokuGrid')
+const Sudoku = require('../datatypes/sudoku')
 
 class App extends Component {
     render() {
-        return h(SudokuGrid, {
-
-        })
+        return h(SudokuGrid, {sudoku: new Sudoku().solve()})
     }
 }
 
--- components/SudokuGrid.js
+++ components/SudokuGrid.js
@@ -1,8 +1,18 @@
 const {h, Component} = require('preact')
 
 class SudokuGrid extends Component {
-    render() {
-        return h('h1', {}, 'Hello World!')
+    render({sudoku}) {
+        return h('section', {id: 'sudoku-grid'},
+            [...Array(9)].map((_, y) =>
+                h('ol', {},
+                    [...Array(9)].map((__, x) =>
+                        h('li', {class: `pos-${x}-${y}`},
+                            h('div', {}, sudoku.arrangement[[x, y]])
+                        )
+                    )
+                )
+            )
+        )
     }
 }
 
