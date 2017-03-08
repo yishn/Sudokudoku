74ccc3d4bfbba3c7cf3b701dd79e31987b7013c0
Yichuan Shen <shen.yichuan@gmail.com>
2017-03-08 21:23:21 +0100

    Add Sudoku.isValid()

--- datatypes/sudoku.js
+++ datatypes/sudoku.js
@@ -1,5 +1,7 @@
 let range = n => [...Array(n)].map((_, i) => i)
 let tuples = arr => arr.map(x => arr.map(y => [x, y])).reduce((sum, x) => [...sum, ...x], [])
+let hasDuplicates = arr => [...arr].sort().some((x, i) => i === 0 || arr[i - 1] === x)
+let equals = v => w => v[0] === w[0] && v[1] === w[1]
 let grid = tuples(range(9))
 
 class Sudoku {
@@ -8,11 +10,17 @@ class Sudoku {
         this.excluded = {}
         this.solids = {}
 
-        for (let [x, y] of grid) {
-            this.arrangement[[x, y]] = null
-            this.excluded[[x, y]] = []
-            this.solids[[x, y]] = null
+        for (let v of grid) {
+            this.arrangement[v] = null
+            this.excluded[v] = []
+            this.solids[v] = null
         }
+    }
+
+    isValid() {
+        return this.getBoxes().every(box =>
+            box.map(v => !hasDuplicates(this.arrangement[v].filter(x => x !== null)))
+        )
     }
 
     getBoxes(vertex = null) {
@@ -25,19 +33,19 @@ class Sudoku {
             ...tuples([0, 3, 6]).map(([tx, ty]) =>
                 range(3).map(i => range(3).map(j => [i + ty, j + tx]))
             )
-        ].filter(box => !vertex || box.some(v => v[0] == vertex[0] && v[1] == vertex[1]))
+        ].filter(box => !vertex || box.some(equals(vertex))
     }
 
     getTrivialMarkup() {
         let result = {}
 
-        for (let [x, y] of grid) {
-            result[[x, y]] = range(9).map(i => i + 1)
+        for (let v of grid) {
+            result[v] = range(9).map(i => i + 1)
 
-            for (let box of this.getBoxes([x, y])) {
-                for (let [bx, by] of box) {
-                    let index = result[[x, y]].indexOf(this.arrangement[[bx, by]])
-                    if (index >= 0) result[[x, y]].splice(index, 1)
+            for (let box of this.getBoxes(v)) {
+                for (let w of box) {
+                    let index = result[v].indexOf(this.arrangement[w])
+                    if (index >= 0) result[v].splice(index, 1)
                 }
             }
         }
